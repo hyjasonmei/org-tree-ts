2a7c921da29727bdfe49db559ec655cd354d7bc2
Jason Mei <s0922380@gmail.com>
2023-10-05 13:48:11 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import OrgTreeComponent, {
+  useTree,
+} from "./local_packages/react-drag-hierarchy-tree/src";
 
 function App() {
+  const data = {
+    id: 1,
+    label: "President",
+    children: [
+      {
+        id: 2,
+        label: "Administrative",
+        children: [
+          {
+            id: 3,
+            label: "Director",
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 2,
+        label: "Administrative",
+        children: [
+          {
+            id: 3,
+            label: "Director",
+            children: [],
+          },
+        ],
+      },
+    ],
+  };
+
+  const { treeRef } = useTree();
+
+  const onClick = () => {
+    treeRef.current?.onExpandNodes();
+  };
+
+  useEffect(() => {
+    console.log(treeRef.current);
+  });
+
   return (
     <div className="App">
+      <button onClick={onClick}>close/open</button>
+      <OrgTreeComponent data={data} ref={treeRef} horizontal />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
